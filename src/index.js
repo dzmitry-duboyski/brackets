bd70c9f64451f685c906643a1ce2ef95389293f4
Dzmitry Duboyski <dzmitry.duboyski@gmail.com>
2021-03-03 17:22:40 +0300

    feat: solved the task, (the algoritm is not optimal, and does not take into accound the number of elements, but it works)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // templatesDeleteString =['()', '[]', '{}', '||'];
+  const templatesDeleteString = bracketsConfig.map((el,i) => el.join(''))
+    let newString =str;
+    for(i=0;i<100;i++){
+            for(j=0;j<templatesDeleteString.length;j++){
+                newString = newString.toString().replace(templatesDeleteString[j],'')
+            }
+    }
+    if (newString) return false
+    if (!newString)return true
 }
